be8bc3304726e127a7a0250897b4ef02b1609efa
Christoph Krey <c@ckrey.de>
2019-04-05 11:46:26 +0200

    [NEW] screenshots [NEW] upload enabled when trip selected only [NEW] Button text Clip

--- SimRa/MyTripsTVC.m
+++ SimRa/MyTripsTVC.m
@@ -29,6 +29,10 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    self.navigationItem.rightBarButtonItem.enabled = FALSE;
+    if (self.tableView.indexPathForSelectedRow) {
+        [self.tableView deselectRowAtIndexPath:self.tableView.indexPathForSelectedRow animated:FALSE];
+    }
     [self.tableView reloadData];
 }
 #pragma mark - Table view data source
@@ -88,6 +92,9 @@
     return YES;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.navigationItem.rightBarButtonItem.enabled = TRUE;
+}
 
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
