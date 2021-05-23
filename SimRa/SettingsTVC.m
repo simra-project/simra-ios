cc25a78dc35b95d76c0b23ff59269810e0a5b2a0
Tobias Reh <14293962+treh@users.noreply.github.com>
2021-05-23 23:53:01 +0200

    [CLEANUP] Code reuse

--- SimRa/SettingsTVC.m
+++ SimRa/SettingsTVC.m
@@ -233,32 +233,14 @@
     [self.ac addAction:aac];
 
     AppDelegate *ad = [AppDelegate sharedDelegate];
-    if (ad.regions.closestsRegions.count > 0) {
-        UIAlertAction *aa0 = [UIAlertAction actionWithTitle:[ad.regions.closestsRegions[0] localizedDescription]
+    for (NSInteger n=0; n<MIN(ad.regions.closestsRegions.count, 3); ++n) {
+        UIAlertAction *aa = [UIAlertAction actionWithTitle:[ad.regions.closestsRegions[n] localizedDescription]
                                                       style:UIAlertActionStyleDefault
                                                     handler:^(UIAlertAction * _Nonnull action) {
-            [ad.regions selectPosition:ad.regions.closestsRegions[0].position];
+            [ad.regions selectPosition:ad.regions.closestsRegions[n].position];
             [self update];
         }];
-        [self.ac addAction:aa0];
-    }
-    if (ad.regions.closestsRegions.count > 1) {
-        UIAlertAction *aa1 = [UIAlertAction actionWithTitle:[ad.regions.closestsRegions[1] localizedDescription]
-                                                      style:UIAlertActionStyleDefault
-                                                    handler:^(UIAlertAction * _Nonnull action) {
-            [ad.regions selectPosition:ad.regions.closestsRegions[1].position];
-            [self update];
-        }];
-        [self.ac addAction:aa1];
-    }
-    if (ad.regions.closestsRegions.count > 2) {
-        UIAlertAction *aa2 = [UIAlertAction actionWithTitle:[ad.regions.closestsRegions[2] localizedDescription]
-                                                      style:UIAlertActionStyleDefault
-                                                    handler:^(UIAlertAction * _Nonnull action) {
-            [ad.regions selectPosition:ad.regions.closestsRegions[2].position];
-            [self update];
-        }];
-        [self.ac addAction:aa2];
+        [self.ac addAction:aa];
     }
 
     [self presentViewController:self.ac animated:TRUE completion:nil];
