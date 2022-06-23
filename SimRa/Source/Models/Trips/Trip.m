0fa0441ba17ede5484d18a3190d83e1524f08453
Christoph Krey <c@ckrey.de>
2022-06-23 14:11:31 +0200

    [FIX] undo round, as it does not solve the problem

--- SimRa/Source/Models/Trips/Trip.m
+++ SimRa/Source/Models/Trips/Trip.m
@@ -699,7 +699,7 @@
                  0.0,
                  0.0,
                  0.0,
-                 round(tripLocation.location.timestamp.timeIntervalSince1970) * 1000.0];
+                 tripLocation.location.timestamp.timeIntervalSince1970 * 1000.0];
     
     if (horizontalAccuracy == -1.0) {
         csvString = [csvString stringByAppendingString:@","];
@@ -875,7 +875,7 @@
                                                         gyro:tripLocation.gyro
                                                closePassInfo:tripLocation.closePassInfo];
             NSNumber *locationTime = [NSNumber numberWithDouble:
-                                      round(tripLocation.location.timestamp.timeIntervalSince1970) * 1000.0];
+                                     tripLocation.location.timestamp.timeIntervalSince1970 * 1000.0];
             locationLines[locationTime] = csvString;
         }
         
